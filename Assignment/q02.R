710e5011de06c46ae03c68a9ce85386311a5e297
Moksh Patel <86453841+moksh5042@users.noreply.github.com>
2025-01-31 15:12:01 +0530

    Add files via upload

---
+++ Assignment/q02.R
@@ -0,0 +1,6 @@
+#1. Create and store a sequence of values from 5 to -11 that progresses in steps of 0.3.
+a<-seq(5,-11,-0.3)
+print(a)
+
+#2. Overwrite the object from (1.) using the same sequence with the order reversed.
+print(rev(a))
